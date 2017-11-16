25c7595d063c836a8663828cdc6ebfa3d13e35c5
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2017-11-16 02:49:49 +0200

    //mvn

--- shapes/src/com/shapes/App.java
+++ shapes/src/com/shapes/App.java
@@ -1,6 +1,6 @@
 package com.shapes;
 
-public class Main {
+public class App{
 
     private static void drawRectangle(int n,int m){
         for(int i=1;i<=n;i++){
@@ -43,10 +43,10 @@ public class Main {
         int k=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                    if(i==n/2+1)System.out.print("*");
-                    else
-                    if(j==n/2+k+1||j==n/2-k+1)System.out.print("*");
-                    else System.out.print(" ");
+                if(i==n/2+1)System.out.print("*");
+                else
+                if(j==n/2+k+1||j==n/2-k+1)System.out.print("*");
+                else System.out.print(" ");
 
 
             }
@@ -56,12 +56,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	    drawRectangle(6,4);
-	    System.out.println();
-	    drawRightTriangle(6);
-	    System.out.println();
-	    drawRegularTriangle(7);
-	    System.out.println();
-	    drawRhomb(7);
+        drawRectangle(6,4);
+        System.out.println();
+        drawRightTriangle(6);
+        System.out.println();
+        drawRegularTriangle(7);
+        System.out.println();
+        drawRhomb(7);
     }
 }
